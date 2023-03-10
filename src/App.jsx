5bec709d7fe816c3875d1f78d67dd4c868cc4a22
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-03-09 20:21:22 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/index.scss'
+import Documentation from './pages/Documentation'
+
+function App() {
+  return (
+    <div className="App">
+      <Documentation />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Documentation.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { Octokit } from "@octokit/core";
+
+export default function Documentation() {
+  const octokit = new Octokit();
+
+  const [MD, setMD] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/PankratzLab/Genvisis-Docs/main/README.md"
+      );
+      const data = await response.text();
+      const githubMD = await octokit.request("POST /markdown", {
+        text: data,
+      });
+      setMD(githubMD.data);
+    })();
+  }, []);
+
+  return (
+    <main className="documentation">
+      <div dangerouslySetInnerHTML={{ __html: MD }}></div>
+    </main>
+  );
+}
