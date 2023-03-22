08a934eab96b4e9966048199bafbf64f15dc78c8
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-03-22 18:05:08 -0500

    fixed content block styling

--- src/pages/Documentation.jsx
+++ src/pages/Documentation.jsx
@@ -4,56 +4,50 @@ import { Octokit } from "@octokit/core";
 
 export default function Documentation() {
   const [MD, setMD] = useState([]);
-  const parsedHTML = useRef();
 
   useEffect(() => {
-    const octokit = new Octokit({
-      auth: process.env.REACT_APP_GITHUB,
-    });
     (async () => {
       const response = await fetch(
-        "/documentation.md"
+        "/documentation.html"
       );
       const data = await response.text();
-      const githubMD = await octokit.request("POST /markdown", {
-        text: data,
-      });
-      console.log(githubMD.data)
       const blockQuoteRegexStart = /(<blockquote[^>]+>|<blockquote>)/g;
       const blockQuoteRegexEnd = /(<\/blockquote>)/g;
-      const newDataStart = githubMD.data.replace(
+      const newDataStart = data.replace(
         blockQuoteRegexStart,
         '<div class="block">'
       );
       const newData = newDataStart.replace(blockQuoteRegexEnd, "</div>");
       setMD(newData);
-
-      function insertBlock(text, type) {
-        const html = ReactDOMServer.renderToStaticMarkup(
-          <div className="block">
-            <div className={`${type.replace(/\s/g, '')}Header`}>{type}</div>
-            <p>{text}</p>
-          </div>
-        )
-        return html
-      }
-
-      const dataHTML = parsedHTML.current.querySelectorAll("div.block");
-      for (let i = 0; i < dataHTML.length; i++) {
-        if (dataHTML[i].innerText.slice(0, 4) === "NOTE") {
-          dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(4), 'Note'))
-          dataHTML[i].remove()
-        } else if (dataHTML[i].innerText.slice(0, 7) === "SEEALSO") {
-          dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(7), 'See Also'))
-          dataHTML[i].remove()
-        } else if (dataHTML[i].innerText.slice(0, 7) === "WARNING") {
-          dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(7), 'Warning'))
-          dataHTML[i].remove()
-        }
-        
-      }
     })();
   }, []);
+
+  const parsedHTML = useRef();
+  useEffect(() => {
+    function insertBlock(text, type) {
+      const html = ReactDOMServer.renderToStaticMarkup(
+        <div className="block">
+          <div className={`${type.replace(/\s/g, '')}Header`}>{type}</div>
+          <p>{text}</p>
+        </div>
+      )
+      return html
+    }
+
+    const dataHTML = parsedHTML.current.querySelectorAll("div.block");
+    for (let i = 0; i < dataHTML.length; i++) {
+      if (dataHTML[i].innerText.slice(0, 4) === "NOTE") {
+        dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(4), 'Note'))
+        dataHTML[i].remove()
+      } else if (dataHTML[i].innerText.slice(0, 7) === "SEEALSO") {
+        dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(7), 'See Also'))
+        dataHTML[i].remove()
+      } else if (dataHTML[i].innerText.slice(0, 7) === "WARNING") {
+        dataHTML[i].insertAdjacentHTML('beforebegin', insertBlock(dataHTML[i].innerText.slice(7), 'Warning'))
+        dataHTML[i].remove()
+      }
+    }
+  }, [MD])
 
   return (
     <main className="documentation">
